40c536f25e1462f4bff1f72cf8a6194c7eb78e6b
Rasheed Amir <rasheed@aurorasolutions.io>
2017-04-29 18:11:22 +0200

    add application web xml

---
+++ gateway/src/main/java/io/as/todo/gateway/AppConstants.java
@@ -0,0 +1,20 @@
+package io.as.todo.gateway;
+
+/**
+ * Application constants.
+ */
+public final class AppConstants
+{
+
+    private AppConstants()
+    {
+    }
+
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+    // to be used during tests; with some stubbed implementations; and in memory database
+    public static final String SPRING_PROFILE_TEST = "test";
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+    public static final String SPRING_PROFILE_FAST = "fast";
+    public static final String SYSTEM_ACCOUNT = "system";
+
+}
