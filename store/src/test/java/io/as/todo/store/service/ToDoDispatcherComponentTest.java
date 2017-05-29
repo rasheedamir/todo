f06926ce6678119cec0a6fa271b0c00d2b4a3939
Rasheed Amir <rasheed@aurorasolutions.io>
2017-05-29 15:36:13 +0200

    add assertion to validate the item in database

--- store/src/test/java/io/as/todo/store/service/ToDoDispatcherComponentTest.java
+++ store/src/test/java/io/as/todo/store/service/ToDoDispatcherComponentTest.java
@@ -3,22 +3,33 @@ package io.as.todo.store.service;
 import io.as.todo.core.domain.ToDo;
 import io.as.todo.store.ComponentTest;
 import io.as.todo.store.ComponentTestBase;
+import io.as.todo.store.repository.ToDoStoreRepository;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Category(ComponentTest.class)
 public class ToDoDispatcherComponentTest extends ComponentTestBase
 {
-    // run embedded mongodb
-    // run embedded kafka
     @Autowired
     private ToDoDispatcher toDoDispatcher;
+
+    @Autowired
+    private ToDoStoreRepository toDoStoreRepository;
 
     @Test
     public void should_dispatch_todo()
     {
-        ToDo toDo = ToDo.newBuilder().title("Quang ToDo").build();
-        toDoDispatcher.dispatch(toDo);
+        // given:
+        ToDo expectedToDo = ToDo.newBuilder().title("Quang ToDo").build();
+
+        // when:
+        toDoDispatcher.dispatch(expectedToDo);
+
+        // then:
+        ToDo actualToDo = toDoStoreRepository.findOne(expectedToDo.getId());
+        assertThat(actualToDo).isEqualTo(expectedToDo);
     }
 }
