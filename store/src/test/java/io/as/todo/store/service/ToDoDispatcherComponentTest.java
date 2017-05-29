d9ec218bacbe3658530e77ac9f265a9a38486045
Rasheed Amir <rasheed@aurorasolutions.io>
2017-05-29 17:50:55 +0200

    verify the msg is sent and recieved on kafka

--- store/src/test/java/io/as/todo/store/service/ToDoDispatcherComponentTest.java
+++ store/src/test/java/io/as/todo/store/service/ToDoDispatcherComponentTest.java
@@ -1,12 +1,20 @@
 package io.as.todo.store.service;
 
+import io.as.todo.core.domain.EntityEvent;
 import io.as.todo.core.domain.ToDo;
 import io.as.todo.store.ComponentTest;
 import io.as.todo.store.ComponentTestBase;
 import io.as.todo.store.repository.ToDoStoreRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,8 +27,11 @@ public class ToDoDispatcherComponentTest extends ComponentTestBase
     @Autowired
     private ToDoStoreRepository toDoStoreRepository;
 
+    @Autowired
+    private Receiver receiver;
+
     @Test
-    public void should_dispatch_todo()
+    public void should_dispatch_todo() throws Exception
     {
         // given:
         ToDo expectedToDo = ToDo.newBuilder().title("Quang ToDo").build();
@@ -29,7 +40,31 @@ public class ToDoDispatcherComponentTest extends ComponentTestBase
         toDoDispatcher.dispatch(expectedToDo);
 
         // then:
+        // verify its saved in the database
         ToDo actualToDo = toDoStoreRepository.findOne(expectedToDo.getId());
         assertThat(actualToDo).isEqualTo(expectedToDo);
+        // verify its sent on kafka
+        receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+        assertThat(receiver.getLatch().getCount()).isEqualTo(0); // check that the message was received
+    }
+}
+
+@Service
+class Receiver
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public CountDownLatch getLatch()
+    {
+        return latch;
+    }
+
+    @KafkaListener(topics = "${todo.kafka.todo-topic}")
+    public void receive(EntityEvent message)
+    {
+        LOGGER.info("received message='{}'", message);
+        latch.countDown();
     }
 }
